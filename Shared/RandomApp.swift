2a602bdcf2849fc175e192e333a51d75b736d0ae
berry <berry3085@kimo.com>
2022-01-28 23:44:36 +0800

    Initial Commit

---
+++ Shared/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Shared
+//
+//  Created by 沈秉叡 on 2022/1/28.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
